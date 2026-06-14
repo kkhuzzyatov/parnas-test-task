4e499e313e1483e534e58059b9071ded0f821842
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-06-14 22:38:25 +0300

    feat(order): добавить кастомный запрос

--- order/src/main/java/parnastesttask/order/api/order/repository/OrderRepository.java
+++ order/src/main/java/parnastesttask/order/api/order/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package parnastesttask.order.api.order.repository;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import parnastesttask.order.api.order.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
@@ -14,4 +17,13 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
   @EntityGraph(attributePaths = "items")
   Optional<Order> findWithItemsById(UUID id);
+
+  @Query(
+      """
+      SELECT COALESCE(SUM(oi.price * oi.quantity), 0)
+      FROM Order o
+      JOIN o.items oi
+      WHERE o.customerName = :customerName
+      """)
+  BigDecimal getTotalOrdersAmountByCustomerName(@Param("customerName") String customerName);
 }
