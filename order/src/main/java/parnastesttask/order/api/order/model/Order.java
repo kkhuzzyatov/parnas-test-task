e5d9d3f3fe402a333075d635479f15c28f5ddf5f
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-06-14 18:28:59 +0300

    refactor(order): улучшить модель

--- order/src/main/java/parnastesttask/order/api/order/model/Order.java
+++ order/src/main/java/parnastesttask/order/api/order/model/Order.java
@@ -5,12 +5,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -22,14 +20,12 @@ public class Order {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  @Setter(AccessLevel.NONE)
   private UUID id;
 
   @Column(name = "customer_name", nullable = false)
   private String customerName;
 
   @Column(name = "order_date", nullable = false)
-  @Setter(AccessLevel.NONE)
   private LocalDateTime orderDate;
 
   @Enumerated(EnumType.STRING)
@@ -41,8 +37,6 @@ public class Order {
       mappedBy = "order",
       cascade = CascadeType.ALL,
       orphanRemoval = true)
-  @Builder.Default
-  @Setter(AccessLevel.NONE)
   private List<OrderItem> items = new ArrayList<>();
 
   public enum Status {
@@ -50,10 +44,6 @@ public class Order {
     PROCESSING,
     COMPLETED,
     CANCELED
-  }
-
-  public void changeCustomerName(String customerName) {
-    this.customerName = customerName;
   }
 
   public void changeStatus(Status status) {
