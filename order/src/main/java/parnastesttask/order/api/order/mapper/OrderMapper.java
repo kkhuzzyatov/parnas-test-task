efa9c9fcb46b02ffe29f60d9efc4179750c63bc0
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-06-14 18:29:39 +0300

    refactor(order): изменить mapper после разделения dto

--- order/src/main/java/parnastesttask/order/api/order/mapper/OrderMapper.java
+++ order/src/main/java/parnastesttask/order/api/order/mapper/OrderMapper.java
@@ -1,26 +1,23 @@
 package parnastesttask.order.api.order.mapper;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import parnastesttask.order.api.order.controller.dto.CreateOrderItemRequest;
 import parnastesttask.order.api.order.controller.dto.CreateOrderRequest;
+import parnastesttask.order.api.order.controller.dto.OrderDetailResponse;
 import parnastesttask.order.api.order.controller.dto.OrderItemResponse;
-import parnastesttask.order.api.order.controller.dto.OrderResponse;
+import parnastesttask.order.api.order.controller.dto.OrderWithoutDetailResponse;
 import parnastesttask.order.api.order.model.Order;
 import parnastesttask.order.api.order.model.OrderItem;
 
 @Component
-@RequiredArgsConstructor
 public class OrderMapper {
 
   public Order toEntity(CreateOrderRequest request) {
     Order order =
         Order.builder().customerName(request.customerName()).status(Order.Status.CREATED).build();
 
-    List<OrderItem> items = request.items().stream().map(this::toEntity).toList();
-
-    items.forEach(order::addItem);
+    request.items().stream().map(this::toEntity).forEach(order::addItem);
 
     return order;
   }
@@ -33,20 +30,39 @@ public class OrderMapper {
         .build();
   }
 
-  public OrderResponse toResponse(Order order) {
-    return OrderResponse.builder()
+  public OrderWithoutDetailResponse toListResponse(Order order) {
+    return OrderWithoutDetailResponse.builder()
         .orderId(order.getId().toString())
         .customerName(order.getCustomerName())
-        .items(order.getItems().stream().map(this::toResponse).toList())
+        .orderDate(order.getOrderDate())
+        .status(order.getStatus().name())
         .build();
   }
 
-  public OrderItemResponse toResponse(OrderItem orderItem) {
-    return OrderItemResponse.builder()
-        .orderItemId(orderItem.getId())
-        .productName(orderItem.getProductName())
-        .quantity(orderItem.getQuantity())
-        .price(orderItem.getPrice())
+  public List<OrderWithoutDetailResponse> toListResponse(List<Order> orders) {
+    return orders.stream().map(this::toListResponse).toList();
+  }
+
+  public OrderDetailResponse toDetailResponse(Order order) {
+    return OrderDetailResponse.builder()
+        .orderId(order.getId().toString())
+        .customerName(order.getCustomerName())
+        .orderDate(order.getOrderDate())
+        .status(order.getStatus().name())
+        .items(toItemResponses(order.getItems()))
         .build();
+  }
+
+  public OrderItemResponse toItemResponse(OrderItem item) {
+    return OrderItemResponse.builder()
+        .orderItemId(item.getId())
+        .productName(item.getProductName())
+        .quantity(item.getQuantity())
+        .price(item.getPrice())
+        .build();
+  }
+
+  public List<OrderItemResponse> toItemResponses(List<OrderItem> items) {
+    return items.stream().map(this::toItemResponse).toList();
   }
 }
