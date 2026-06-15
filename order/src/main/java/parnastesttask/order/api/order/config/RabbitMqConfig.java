dc47b67b4aef13f1cb379fe24b4bd5e47f11ebfa
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-06-15 15:59:12 +0300

    refactor(order): изменить json converter для rabbitMq

--- order/src/main/java/parnastesttask/order/api/order/config/RabbitMqConfig.java
+++ order/src/main/java/parnastesttask/order/api/order/config/RabbitMqConfig.java
@@ -1,16 +1,12 @@
 package parnastesttask.order.api.order.config;
 
-import com.google.gson.Gson;
-import java.nio.charset.StandardCharsets;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.MessageConversionException;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -40,46 +36,24 @@ public class RabbitMqConfig {
 
   @Bean
   public Binding orderCreatedBinding() {
-    return BindingBuilder.bind(orderCreatedQueue()).to(orderExchange()).with(routingKey);
+    return BindingBuilder
+            .bind(orderCreatedQueue())
+            .to(orderExchange())
+            .with(routingKey);
   }
 
   @Bean
-  public Gson gson() {
-    return new Gson();
-  }
-
-  @Bean
-  public MessageConverter messageConverter(Gson gson) {
-    return new MessageConverter() {
-
-      @Override
-      public Message toMessage(Object object, MessageProperties messageProperties)
-          throws MessageConversionException {
-
-        String json = gson.toJson(object);
-
-        messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        messageProperties.setContentEncoding(StandardCharsets.UTF_8.name());
-
-        return new Message(json.getBytes(StandardCharsets.UTF_8), messageProperties);
-      }
-
-      @Override
-      public Object fromMessage(Message message) throws MessageConversionException {
-
-        String json = new String(message.getBody(), StandardCharsets.UTF_8);
-
-        return gson.fromJson(json, Object.class);
-      }
-    };
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 
   @Bean
   public RabbitTemplate rabbitTemplate(
-      ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+          ConnectionFactory connectionFactory,
+          MessageConverter messageConverter
+  ) {
     RabbitTemplate template = new RabbitTemplate(connectionFactory);
     template.setMessageConverter(messageConverter);
-
     return template;
   }
 }
